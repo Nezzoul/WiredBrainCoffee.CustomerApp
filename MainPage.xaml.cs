7f266a4c1474720809a84869ef19de717e1b0ad9
Marcus Bernhard <Marcus.bernhard@outlook.com>
2019-12-18 16:28:18 +0100

    start of branches changed places with code

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WiredBrainCoffee.CustomerApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Customer added!");
+            await messageDialog.ShowAsync();
+        }
+
+        private void ButtonDeleteCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ButtonMove_Click(object sender, RoutedEventArgs e)
+        {
+            int column = Grid.GetColumn(customerListGrid);
+
+            int newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(customerListGrid, newColumn);
+
+            moveSymbolIcon.Symbol = newColumn == 0 ? Symbol.Forward : Symbol.Back;
+        }
+    }
+}
